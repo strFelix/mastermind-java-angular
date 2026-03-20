3cf191648ff1ad241cef38116cb279aa46339e09
Lucas Felix <Lucas.felixandrade@hotmail.com.br>
2026-03-20 00:25:33 -0300

    feat: add CORS config

---
+++ mastermind-spring/src/main/java/br/com/strfelix/mastermind_spring/config/security/CorsConfig.java
@@ -0,0 +1,25 @@
+package br.com.strfelix.mastermind_spring.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Profile("dev")
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
