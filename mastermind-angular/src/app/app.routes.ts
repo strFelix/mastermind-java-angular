338089dbabcd0d0ee99803fd62ce56d011df06a7
Lucas Felix <Lucas.felixandrade@hotmail.com.br>
2026-03-24 01:40:45 -0300

    feat: add auth guard to protect private routes

--- mastermind-angular/src/app/app.routes.ts
+++ mastermind-angular/src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { Dashboard } from './pages/dashboard/dashboard';
-import { Ranking } from './pages/ranking/ranking';
 import { Game } from './pages/game/game';
+import { Ranking } from './pages/ranking/ranking';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', component: Login },
   { path: 'register', component: Register },
-  { path: 'dashboard', component: Dashboard },
-  { path: 'ranking', component: Ranking },
-  { path: 'game', component: Game },
+  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+  { path: 'game',      component: Game,      canActivate: [authGuard] },
+  { path: 'ranking',   component: Ranking,   canActivate: [authGuard] },
 ];
